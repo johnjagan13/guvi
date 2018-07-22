d59dbbf337299188b53e91c21b95e30a0f207819
johnjagan13 <34595251+johnjagan13@users.noreply.github.com>
2018-07-22 11:27:44 +0530

    Create candy_assignment(pro16).py

---
+++ candy_assignment(pro16).py
@@ -0,0 +1,10 @@
+n=int(input())
+l=list(map(int,input().split()))
+l1=[]
+for i in range(0,len(l)):
+	l1.append(1)
+for i in range(1,len(l)):
+	if(l[i]>l[i-1]):
+		while(l1[i]<=l1[i-1]):
+			l1[i]+=1
+print(sum(l1))
