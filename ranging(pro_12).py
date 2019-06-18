37af01abf3717aa1e521d0e00960f8d24e819bd2
Jagan <34595251+johnjagan13@users.noreply.github.com>
2019-06-18 16:54:08 +0530

    Update ranging(pro_12).py

--- ranging(pro_12).py
+++ ranging(pro_12).py
@@ -1,12 +1,15 @@
-n,q = input().split()
-n = int(n)
-q = int(q)
-array = [int(x) for x in input().split()]
-for i in range(q):
-	u,v = input().split()
-	u = int(u)-1
-	v = int(v)-1
-	sum = 0
-	for i in range(u,v+1):
-		sum+=array[i]
-	print(sum)
+try:
+	n,q = input().split()
+	n = int(n)
+	q = int(q)
+	array = [int(x) for x in input().split()]
+	for i in range(q):
+		u,v = input().split()
+		u = int(u)-1
+		v = int(v)-1
+		sum = 0
+		for i in range(u,v+1):
+			sum+=array[i]
+		print(sum)
+except:
+	print("Not Accepted")
